f911fa665e6141101d304a0bfbe83bdceeaa4c99
hyeooi <xrgsnim@naver.com>
2023-06-08 22:02:50 +0900

    ...

--- main.py
+++ main.py
@@ -35,21 +35,18 @@ def main():
         [4, 2, 2, 2, 2, 2, 3, 2, 2, 2, 2, 2 ,3, 2, 2, 2, 2, 2, 4]
     ])
     
-    # 3, 9, 15열이면서 블럭이 아닌 임의의 좌표에 YT, Job 생성
-    YT_locations = {0: (4, 9), 1: (2, 3), 2: (2, 3), 3: (0, 3), 4: (6, 9), 5: (6, 9), 6: (6, 3), 
-    7: (4, 9), 8: (6, 15), 9: (2, 15), 10: (2, 15), 11: (2, 3), 12: (2, 15), 13: (4, 15), 14: (2, 9), 15: (4, 15), 16: (4, 3), 17: (4, 9), 18: (6, 3), 19: (2, 3)}
-    Job_locations = {0: [(4, 3), (0, 15)], 1: [(2, 15), (6, 15)], 2: [(2, 3), (8, 9)], 3: [(8, 9), (0, 15)], 4: [(4, 3), (0, 15)], 5: [(0, 3), (6, 3)], 6: [(6, 3), (0, 3)], 7: [(2, 15), (4, 
-    9)], 8: [(4, 9), (0, 9)], 9: [(0, 3), (2, 15)], 10: [(6, 9), (8, 15)], 11: [(4, 3), (4, 15)], 12: [(8, 9), (2, 15)], 13: [(0, 15), (2, 9)], 14: [(6, 3), (6, 15)], 15: [(2, 9), (8, 15)], 
-    16: [(4, 15), (4, 9)], 17: [(8, 9), (8, 3)], 18: [(8, 3), (4, 15)], 19: [(4, 9), (8, 9)]} 
+    YT_locations = {0: (0, 9), 1: (4, 15), 2: (4, 15), 3: (8, 15), 4: (2, 9), 5: (6, 3), 6: (0, 3), 7: (2, 15), 8: (6, 3), 9: (6, 9), 10: (8, 9), 11: (2, 9), 12: (4, 15), 13: (0, 15), 14: (6, 9), 15: (0, 3), 16: (0, 15), 17: (6, 15), 18: (8, 3), 19: (4, 3)}
+    Job_locations = {0: [(6, 9), (6, 3)], 1: [(0, 15), (2, 15)], 2: [(4, 3), (4, 9)], 3: [(0, 3), (2, 9)], 4: [(6, 9), (8, 3)], 5: [(4, 9), (8, 15)], 6: [(4, 9), (0, 3)], 7: [(2, 3), (0, 3)], 8: [(4, 3), (6, 15)], 9: [(4, 9), (8, 9)], 10: [(4, 3), (2, 9)], 11: [(0, 9), (8, 9)], 12: 
+    [(6, 3), (0, 15)], 13: [(2, 9), (8, 9)], 14: [(2, 15), (8, 3)], 15: [(6, 3), (2, 3)], 16: [(6, 15), (2, 15)], 17: [(8, 9), (8, 15)], 18: [(0, 3), (8, 9)], 19: [(4, 9), (6, 3)]}
 
     number_of_YT = 20
     number_of_Job = 20
 
-    # # 스케줄링 대상 YT 생성
+    # 스케줄링 대상 YT 생성
     # YT_locations = {}
     # Job_locations = {}
     
-    # # 3, 9, 15열이면서 블럭이 아닌 임의의 좌표에 YT, Job 생성
+    # 3, 9, 15열이면서 블럭이 아닌 임의의 좌표에 YT, Job 생성
     # for i in range(number_of_YT):
     #     YT_location = None
     #     while YT_location is None or grid[YT_location] == -1 or YT_location[1] not in [3, 9, 15]:
@@ -69,7 +66,7 @@ def main():
     # for i in range(number_of_YT):
     #     YT_location = None
     #     while YT_location is None or grid[YT_location] == -1:
-    #         YT_location = (np.random.randint(9), np.random.randint(7))
+    #         YT_location = (np.random.randint(len(grid)), np.random.randint(len(grid[0])))
     #     YT_locations[i] = YT_location
         
     # # 스케줄링 대상 작업 생성
@@ -77,8 +74,8 @@ def main():
     #     Pick_location = None
     #     Drop_location = None
     #     while Pick_location is None or grid[Pick_location] == -1 or Drop_location is None or grid[Drop_location] == -1 or Pick_location == Drop_location:
-    #         Pick_location = (np.random.randint(9), np.random.randint(7))
-    #         Drop_location = (np.random.randint(9), np.random.randint(7))
+    #         Pick_location = (np.random.randint(len(grid)), np.random.randint(len(grid[0])))
+    #         Drop_location = (np.random.randint(len(grid)), np.random.randint(len(grid[0])))
     #     Job_locations[j] = [Pick_location, Drop_location]
 
 
@@ -87,9 +84,9 @@ def main():
 
 
     number_of_final_route = 3
-    alpha1 = 0.3333 # prev counter
-    alpha2 = 0.3333 # now counter
-    alpha3 = 0.3333 # distance
+    alpha1 = 0.5 # prev counter
+    alpha2 = 0.5 # now counter
+    alpha3 = 0 # distance
     prev_count = np.array([
     [397,  397,  397,  612,  517,  517,  724,  423,  423,  444,  414,  414, 633,  383,  383,  404,  195,  195,  195,],
     [397,    0,    0,    0,    0,    0,  508,    0,    0,    0,    0,    0, 469,    0,    0,    0,    0,    0,  195,],
