ab3c663728d039a516dc01b1314849b4c0a6e7ed
ppurify <qpqp8558@gmail.com>
2023-07-20 17:04:28 +0900

    Change script name StationInfo to CraneInfo

--- Traffic_Simulation/Assets/TrafficSimulation/Scripts/CranesInfo.cs
+++ Traffic_Simulation/Assets/TrafficSimulation/Scripts/CranesInfo.cs
@@ -2,13 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StationsInfo : MonoBehaviour
+public class CranesInfo : MonoBehaviour
 {
 
     // 현재 작업 중인 트럭 개수
-    public int stationStatus;
+    public int craneStatus;
     // 작업 가능한 Max 트럭 개수
-    public int stationCapacity;
+    public int craneCapacity;
 
     // 작업 대기 중인 트럭 리스트
     public List<GameObject> processQueueList;
@@ -26,12 +26,13 @@ public class StationsInfo : MonoBehaviour
 
     void Awake()
     {
-        stationStatus = 0;
-        // Randomly assign 2 or 3 to the 'stationCapacity' variable
-        stationCapacity = Random.Range(2, 4);
+        craneStatus = 0;
+        // Randomly assign 2 or 3 to the 'craneCapacity' variable
+        // craneCapacity = Random.Range(2, 4);
+        AssignCraneCapacity(quayCranePosition_z, 2, 3);
 
         AssignProcessTime(quayCranePosition_z);
-        // stationCapacity = 2;
+        // craneCapacity = 2;
     
         processQueueList = new List<GameObject>();
         processList = new List<GameObject>();
@@ -44,13 +45,29 @@ public class StationsInfo : MonoBehaviour
         // Assign process time to each crane
         if(this.transform.position.z == quayCranePos_z)
         {
-            craneProcessTime = 180f;
+            // craneProcessTime = 180f;
+            craneProcessTime = 127f;
         }
 
         else
         {
-            craneProcessTime = 120f;
+            // craneProcessTime = 120f;
+            craneProcessTime = 90f;
         }
     }
     
+    private void AssignCraneCapacity(float quayCranePos_z, int _quayCraneCapacity, int _yardCraneCapacity)
+    {
+        // Quay crane capacity
+        if(this.transform.position.z == quayCranePos_z)
+        {
+            craneCapacity = _quayCraneCapacity;
+        }
+
+        // Yard crane capacity
+        else
+        {
+            craneCapacity = _yardCraneCapacity;
+        }
+    }
 }
