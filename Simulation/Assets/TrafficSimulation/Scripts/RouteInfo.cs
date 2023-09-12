05988981257da2a533476f1bc79accb933f489d1
ppurify <qpqp8558@gmail.com>
2023-09-12 20:16:49 +0900

    delete scripts folder in Traffic Simulation

--- Simulation/Assets/TrafficSimulation/Scripts/RouteInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class RouteInfo : MonoBehaviour
-{
-    public List<Vector3> uTurnStations = new List<Vector3>();
-}
