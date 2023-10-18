49d09d1e625778af3cfe9270623ed66e71472b82
hyeooi <xrgsnim@naver.com>
2023-10-18 22:07:20 +0900

    run model for experiment for gamma combination

--- Model/main.py
+++ Model/main.py
@@ -128,7 +128,7 @@ def main(_grid, _YT_locations, _Job_locations, number_of_YT, number_of_Job, case
               _next_prev_count[arc.path[i][0]][arc.path[i][1]] += 1
 
   # Create csv file for Unity simulation
-  # make_csv.create_csv(activated_arcs, number_of_YT, _grid, filename_Truck, filename_RoutePoints)
+  make_csv.create_csv(activated_arcs, number_of_YT, _grid, filename_Truck, filename_RoutePoints)
 
 
   # Save Log file
@@ -148,12 +148,12 @@ def main(_grid, _YT_locations, _Job_locations, number_of_YT, number_of_Job, case
 
 
 if __name__ == "__main__":
-  casename = 'Completiontime_Congestionratio'
+  casename = 'gamma_combination'
 
   # Prev_number_list = [5,5,10,10,15,15,20,20,25,25,30]
   # Now_number_list = [5,10,10,15,15,20,20,25,25,30,30]
-  Prev_number_list = [5]
-  Now_number_list = [5]
+  Prev_number_list = [20]
+  Now_number_list = [20]
 
   block_num_in_row = 3
   block_length = 9
@@ -175,19 +175,19 @@ if __name__ == "__main__":
 
     case_folder_path = f'{casename}/prev_{Prev_number_of_YT}_now_{Now_number_of_YT}'
 
-    reps = 1
+    reps = 7
 
     # alphas = [[0, 10, 20, 30, 40, 50, 60, 70, 80],
     #           [0, 80, 70, 60, 50, 40, 30, 20, 10],
     #           [100, 10, 10, 10, 10, 10, 10, 10, 10]]
       
-    alphas = [[20],
-              [70],
-              [10]]
+    alphas = [[40, 20, 10, 10],
+              [30, 20, 10, 0],
+              [30, 60, 80, 90]]
               
     for rep in range(reps):
       
-      rep = rep + 25
+      rep = rep + 1
 
       prev_YT_locations, prev_Job_locations = generate_locations(grid, Prev_number_of_YT, Prev_number_of_Job, YT_location_col_index, QC_locations, YC_locations)
       now_YT_locations, now_Job_locations = generate_locations(grid, Now_number_of_YT, Now_number_of_Job, YT_location_col_index, QC_locations, YC_locations)
